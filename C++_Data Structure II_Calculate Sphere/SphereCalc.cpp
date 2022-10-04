6c91a30c4016196a65d513e5ea23990ae025a0f2
kweont0211 <114732820+kweont0211@users.noreply.github.com>
2022-10-04 10:00:07 +0900

    C++_Data Structure Calculate Sphere

---
+++ C++_Data Structure II_Calculate Sphere/SphereCalc.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int c=3;
+    int*p=nullptr;
+    p= &c;
+    cout<<p<<endl;
+
+    return 0;
+}
---
+++ C++_Data Structure II_Calculate Sphere/SphereCalc.h
@@ -0,0 +1,7 @@
+#ifndef UNTITLED_SPHERECALC_H
+#define UNTITLED_SPHERECALC_H
+using namespace std;
+
+double VolumeOfSphere(double r);
+double AreaOfSphere(double r);
+#endif //UNTITLED_SPHERECALC_H
