2b74b18353a5833993fe08d7a854a0a72feae0b3
Jack(TaeYoun) Kwon <kweont0211@gmail.com>
2023-05-09 21:26:08 +0900

    update

---
+++ 01.Array/Python_List and Tuple Test/sort.py
@@ -0,0 +1,23 @@
+myList = [5,-3,4,-1,2]
+myTuple = (5,-3,4,-1,2)
+# myList = sorted(myList, reverse= True)
+# print(myList)
+
+# myList.sort(key = lambda x : abs(x))
+# print(myList)
+
+# a = myList.sort(reverse=True)
+# print(myList)
+# print(a)
+
+# print(myList)
+# ,key=lambda x : abs(x)
+
+# a = myList.sort() 
+# print(myList)
+# print(a)
+
+a = sorted(myTuple)
+print(a)
+print(myTuple)
+# myTuple.sort()
